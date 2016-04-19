56e6d598ce7bc28bdfac8594677119af30e09e2f
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2016-04-19 11:52:37 -0400

    add gmake notes for redis_fdw

--- build_redis_fdw.sh
+++ build_redis_fdw.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/nahanni/rw_redis_fdw.git
 cd rw_redis_fdw
 
+# NOTE: need to use gmake to build this, FreeBSD default is pmake
+# http://www.freshports.org/devel/gmake/
+
 PATH=/usr/local/lib/postgresql/bin:$PATH gmake
 sudo PATH=/usr/local/lib/postgresql/bin:$PATH gmake install
